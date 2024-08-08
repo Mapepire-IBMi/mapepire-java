877235ed6a14d7d394bff8e9a138a941592fdc92
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-08-08 17:41:46 -0400

    implement pool class
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/main/java/io/github/mapapire/Pool.java
+++ src/main/java/io/github/mapapire/Pool.java
@@ -1,15 +1,20 @@
 package io.github.mapapire;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import io.github.mapapire.types.DaemonServer;
 import io.github.mapapire.types.JDBCOptions;
 import io.github.mapapire.types.JobStatus;
+import io.github.mapapire.types.QueryOptions;
+import io.github.mapapire.types.QueryResult;
 
 class PoolOptions {
     private DaemonServer creds;
@@ -50,10 +55,14 @@ class PoolOptions {
 class PoolAddOptions {
     /** An existing job to add to the pool */
     private SqlJob existingJob;
+
     /** Don't add to the pool */
     private boolean poolIgnore;
 
-    PoolAddOptions(SqlJob _existingJob, boolean _poolIgnore) {
+    public PoolAddOptions() {
+    }
+
+    public PoolAddOptions(SqlJob _existingJob, boolean _poolIgnore) {
         this.existingJob = _existingJob;
         this.poolIgnore = _poolIgnore;
     }
@@ -62,8 +71,16 @@ class PoolAddOptions {
         return existingJob;
     }
 
+    public void setExistingJob(SqlJob existingJob) {
+        this.existingJob = existingJob;
+    }
+
     public boolean isPoolIgnore() {
         return poolIgnore;
+    }
+
+    public void setPoolIgnore(boolean poolIgnore) {
+        this.poolIgnore = poolIgnore;
     }
 }
 
@@ -88,7 +105,7 @@ public class Pool {
 
     public boolean hasSpace() {
         int activeJobCount = this.jobs.stream()
-                .filter(job -> !INVALID_STATES.contains(job.getStatus()))
+                .filter(j -> !INVALID_STATES.contains(j.getStatus()))
                 .collect(Collectors.toList())
                 .size();
         return activeJobCount < options.getMaxSize();
@@ -96,13 +113,18 @@ public class Pool {
 
     public int getActiveJobCount() {
         return this.jobs.stream()
-                .filter(job -> job.getStatus() == JobStatus.Busy || job.getStatus() == JobStatus.Ready)
+                .filter(j -> j.getStatus() == JobStatus.Busy || j.getStatus() == JobStatus.Ready)
                 .collect(Collectors.toList())
                 .size();
     }
 
     public void cleanup() {
-        this.jobs.removeIf(job -> INVALID_STATES.contains(job.getStatus()));
+        this.jobs.removeIf(j -> INVALID_STATES.contains(j.getStatus()));
+    }
+
+    public CompletableFuture<SqlJob> addJob() {
+        PoolAddOptions options = new PoolAddOptions();
+        return this.addJob(options);
     }
 
     // TODO: test cases with existingJob parameter
@@ -111,7 +133,8 @@ public class Pool {
             cleanup();
         }
 
-        SqlJob newSqlJob = options.getExistingJob() != null ? options.getExistingJob() : new SqlJob(this.options.getOpts());
+        SqlJob newSqlJob = options.getExistingJob() != null ? options.getExistingJob()
+                : new SqlJob(this.options.getOpts());
 
         if (options.isPoolIgnore() != true) {
             this.jobs.add(newSqlJob);
@@ -128,5 +151,116 @@ public class Pool {
         }
 
         return CompletableFuture.completedFuture(newSqlJob);
+    }
+
+    public SqlJob getReadyJob() {
+        return this.jobs
+                .stream()
+                .filter(j -> j.getStatus() == JobStatus.Ready)
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getReadyJobIndex() {
+        return IntStream.range(0, this.jobs.size())
+                .filter(j -> jobs.get(j).getStatus() == JobStatus.Ready)
+                .findFirst()
+                .orElse(-1);
+    }
+
+    /**
+     * Returns a job as fast as possible. It will either be a ready job
+     * or the job with the least requests on the queue. Will spawn new jobs
+     * if the pool is not full but all jobs are busy.
+     */
+    public SqlJob getJob() {
+        SqlJob job = this.getReadyJob();
+        if (job == null) {
+            // This code finds a job that is busy, but has the least requests on the queue
+            List<SqlJob> busyJobs = this.jobs
+                    .stream()
+                    .filter(j -> j.getStatus() == JobStatus.Busy)
+                    .collect(Collectors.toList());
+            busyJobs
+                    .sort(Comparator.comparingInt(j -> j.getRunningCount()));
+            SqlJob freeist = busyJobs.isEmpty() ? null : busyJobs.get(0);
+
+            // If this job is busy, and the pool is not full, add a new job for later
+            if (this.hasSpace() && freeist != null && freeist.getRunningCount() > 2) {
+                this.addJob();
+            }
+
+            return freeist;
+        }
+
+        return job;
+    }
+
+    public CompletableFuture<SqlJob> waitForJob() {
+        return this.waitForJob(false);
+    }
+
+    /**
+     * Returns a ready job if one is available, otherwise it will add a new job.
+     * If the pool is full, then it will find a job with the least requests on the
+     * queue.
+     */
+    public CompletableFuture<SqlJob> waitForJob(boolean useNewJob) {
+        SqlJob job = getReadyJob();
+
+        if (job == null) {
+            if (hasSpace() || useNewJob) {
+                return addJob().thenApply(newJob -> {
+                    return newJob;
+                });
+            } else {
+                return CompletableFuture.completedFuture(this.getJob());
+            }
+        }
+
+        return CompletableFuture.completedFuture(job);
+    }
+
+    // TODO: needs test cases
+    /**
+     * Returns a job that is ready to be used. If no jobs are ready, it will
+     * create a new job and return that. Use `addJob` to add back to the pool.
+     */
+    public CompletableFuture<SqlJob> popJob() {
+        int index = getReadyJobIndex();
+        if (index > -1) {
+            return CompletableFuture.completedFuture(jobs.remove(index));
+        } else {
+            // Add a new job asynchronously
+            PoolAddOptions options = new PoolAddOptions();
+            options.setPoolIgnore(true);
+            return this.addJob(options);
+        }
+    }
+
+    // TODO: Fix optional parm
+    public <T> Query<T> query(String sql) {
+        return this.query(sql, null);
+    }
+
+    // TODO: Fix optional parm
+    public <T> Query<T> query(String sql, QueryOptions opts) {
+        SqlJob job = this.getJob();
+        return job.query(sql, opts);
+    }
+
+    // TODO: Fix optional parm
+    public <T> CompletableFuture<QueryResult<T>> execute(String sql) {
+        return this.execute(sql, null);
+    }
+
+    // TODO: Fix optional parm
+    public <T> CompletableFuture<QueryResult<T>> execute(String sql, QueryOptions opts) {
+        SqlJob job = this.getJob();
+        return job.execute(sql, opts);
+    }
+
+    public void end() {
+        this.jobs.forEach(j -> j.close());
     }
 }
