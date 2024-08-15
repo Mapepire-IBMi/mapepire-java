1cfe3c9ba4daeb5f40f652d33c8e7ef7d61a4c55
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-08-15 18:06:43 -0400

    Add skeleton for tls tests
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

---
+++ src/test/java/io/github/mapapire/TlsTest.java
@@ -0,0 +1,14 @@
+package io.github.mapapire;
+import org.junit.jupiter.api.Test;
+
+class TlsTest extends MapepireTest {
+    @Test
+    void canGetCertCorrectlyTest() {
+        //TODO:
+    }
+
+    @Test
+    void willFailCorrectlyTest() {
+        //TODO:
+    }
+}
