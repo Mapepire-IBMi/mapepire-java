bf3783f5c82db427103c5eca698e02ef2bdfc466
Jan Frode Håskjold <jan.frode.haskjold@outlook.com>
2024-10-28 10:02:08 +0100

    Added precision and scale to column metadata

--- src/main/java/io/github/mapepire_ibmi/types/ColumnMetadata.java
+++ src/main/java/io/github/mapepire_ibmi/types/ColumnMetadata.java
@@ -31,6 +31,18 @@ public class ColumnMetadata {
     private String type;
 
     /**
+     * The precision/length of the column.
+     */
+    @JsonProperty("precision")
+    private int precision;
+
+    /**
+     * The scale of the column.
+     */
+    @JsonProperty("scale")
+    private int scale;
+
+    /**
      * Construct a new ColumnMetadata instance.
      */
     public ColumnMetadata() {
@@ -39,22 +51,26 @@ public class ColumnMetadata {
 
     /**
      * Construct a new ColumnMetadata instance.
-     * 
+     *
      * @param displaySize The display size of the column.
      * @param label       The label of the column.
      * @param name        The name of the column.
      * @param type        The type of the column.
+     * @param precision   The precision/length of the column.
+     * @param scale       The scale of the column.
      */
-    public ColumnMetadata(int displaySize, String label, String name, String type) {
+    public ColumnMetadata(int displaySize, String label, String name, String type, int precision, int scale) {
         this.displaySize = displaySize;
         this.label = label;
         this.name = name;
         this.type = type;
+        this.precision = precision;
+        this.scale = scale;
     }
 
     /**
      * Get the display size of the column.
-     * 
+     *
      * @return The display size of the column.
      */
     public int getDisplaySize() {
@@ -63,7 +79,7 @@ public class ColumnMetadata {
 
     /**
      * Set the display size of the column.
-     * 
+     *
      * @param displaySize The display size of the column.
      */
     public void setDisplaySize(int displaySize) {
@@ -72,7 +88,7 @@ public class ColumnMetadata {
 
     /**
      * Get the label of the column.
-     * 
+     *
      * @return The label of the column.
      */
     public String getLabel() {
@@ -81,7 +97,7 @@ public class ColumnMetadata {
 
     /**
      * Set the label of the column.
-     * 
+     *
      * @param label The label of the column.
      */
     public void setLabel(String label) {
@@ -90,7 +106,7 @@ public class ColumnMetadata {
 
     /**
      * Get the name of the column.
-     * 
+     *
      * @return The name of the column.
      */
     public String getName() {
@@ -99,7 +115,7 @@ public class ColumnMetadata {
 
     /**
      * Set the name of the column.
-     * 
+     *
      * @param name The name of the column.
      */
     public void setName(String name) {
@@ -108,7 +124,7 @@ public class ColumnMetadata {
 
     /**
      * Get the type of the column.
-     * 
+     *
      * @return The type of the column.
      */
     public String getType() {
@@ -117,10 +133,46 @@ public class ColumnMetadata {
 
     /**
      * Set the type of the column.
-     * 
+     *
      * @param type The type of the column.
      */
     public void setType(String type) {
         this.type = type;
+    }
+
+    /**
+     * Get the precision/length of the column.
+     *
+     * @return The precision/length of the column.
+     */
+    public int getPrecision() {
+        return precision;
+    }
+
+    /**
+     * Set the precision/length of the column.
+     *
+     * @param precision The precision/length of the column.
+     */
+    public void setPrecision(int precision) {
+        this.precision = precision;
+    }
+
+    /**
+     * Get the scale of the column.
+     *
+     * @return The scale of the column.
+     */
+    public int getScale() {
+        return scale;
+    }
+
+    /**
+     * Set the scale of the column.
+     *
+     * @param scale The scale of the column.
+     */
+    public void setScale(int scale) {
+        this.scale = scale;
     }
 }
