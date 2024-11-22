e5a9fe106a935d648bdb5889250121fab3b984b7
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-11-22 14:34:46 -0500

    remove test
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/test/java/io/github/mapepire_ibmi/TlsTest.java
+++ src/test/java/io/github/mapepire_ibmi/TlsTest.java
@@ -28,17 +28,4 @@ class TlsTest extends MapepireTest {
 
         assertTrue(e.getMessage().contains("Connection refused: connect"));
     }
-
-    @Test
-    void failsHandshakeWithInvalidCa() throws Exception {
-        ExecutionException e = assertThrowsExactly(ExecutionException.class, () -> {
-            DaemonServer creds = MapepireTest.getCreds();
-            creds.setCa(MapepireTest.invalidCA);
-            SqlJob job = new SqlJob();
-            job.connect(creds).get();
-        });
-
-        assertTrue(e.getMessage().contains(
-                "PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target"));
-    }
 }
