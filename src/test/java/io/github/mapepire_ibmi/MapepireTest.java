f52bdad5c957c9407713ded4cfbc9b726e6ceb4e
Sanjula Ganepola <Sanjula.Ganepola@ibm.com>
2024-11-22 13:42:33 -0500

    Add TLS test cases
    Signed-off-by: Sanjula Ganepola <Sanjula.Ganepola@ibm.com>

--- src/test/java/io/github/mapepire_ibmi/MapepireTest.java
+++ src/test/java/io/github/mapepire_ibmi/MapepireTest.java
@@ -16,16 +16,40 @@ import io.github.mapepire_ibmi.types.DaemonServer;
 
 @Timeout(25)
 class MapepireTest {
-    private static DaemonServer creds;
-    private static DaemonServer invalidCreds;
-    private static final String CONFIG_FILE = "config.properties";
+    private static String host;
+    private static String user;
+    private static String password;
+    private static int port;
+    public static String invalidCA = "-----BEGIN CERTIFICATE-----\n" +
+            "MIIDtjCCAp6gAwIBAgIUaZwqO1YXrSGUZ+j2YlefGD+Li3UwDQYJKoZIhvcNAQEL\n" +
+            "BQAwcjELMAkGA1UEBhMCZHIxCzAJBgNVBAgMAnNlMQwwCgYDVQQHDAN0eXUxDDAK\n" +
+            "BgNVBAoMA3NlcjELMAkGA1UECwwCaGYxDzANBgNVBAMMBmRyZnRnZzEcMBoGCSqG\n" +
+            "SIb3DQEJARYNbG9nQGdtYWlsLmNvbTAeFw0yNDEwMDIxODU3MDFaFw0yNTEwMDIx\n" +
+            "ODU3MDFaMHQxCzAJBgNVBAYTAkNBMQswCQYDVQQIDAJPTjEQMA4GA1UEBwwHVG9y\n" +
+            "b250bzEMMAoGA1UECgwDSUJNMQswCQYDVQQLDAJMTDEMMAoGA1UEAwwDSk9OMR0w\n" +
+            "GwYJKoZIhvcNAQkBFg5mYWtlQGdtYWlsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQAD\n" +
+            "ggEPADCCAQoCggEBAI39BFoVAXIc0HFH7MgDAI53ExKgZkyBXGjFsooyzM/u215h\n" +
+            "QLtsfirmsFU8Kq2HOY1UaB1PRq4nrHYrwO7nYpmA14/9EgEXQsSRDH8LO18sSUQb\n" +
+            "hOX3622CunKDT77O7z5LakPuVvOsj1XBVTyFONTTzcuWW0mcnupj7j+WF+fldV3y\n" +
+            "Z64rfk/wJ2W1FjWNgxtm076KivVrV4RvL7DmGQH5sCENCx4eaGh2LGe1kb5yACQ3\n" +
+            "9zeC9aEwogEh2QRV8x3LzsroF3NR/IqzIm6L3kaiyWTsQkVlztmGpXY3WnFgfoBj\n" +
+            "e6IZOCRXzA9iTS1dRDGnFSzcRawf+PSIbP88LZ0CAwEAAaNCMEAwHQYDVR0OBBYE\n" +
+            "FH1B2+PDJmga5MwzswnukmSEt50OMB8GA1UdIwQYMBaAFPHGkLtIDz/tR3iBLgzU\n" +
+            "DjEK+tsoMA0GCSqGSIb3DQEBCwUAA4IBAQCnlEjRBF+IUNRfYVqOW4uHJriaBViu\n" +
+            "6zdXG+13pa7La+mAZ0BsoP1pLhrWjDul271MTOYsq429XBtlfxaNJiqHuPNjccKa\n" +
+            "wga2NFLAZriHYUvyP4Ld/H0IVAleIem4w2vwqHqayV2GeQCn5H+LknIaTzHKuRZ9\n" +
+            "fv6C/V5jBJFAJ29tYh79lioIRIZ6nzYLGWQIXbh9Y8uNIMbU3z4fqRQN65gKCkBB\n" +
+            "HaelrFfJI+UCGwOnr4qTKxkEB/lNz47O7kh4vmAk4mU3IsSWDMsydFHCTPLMg/Me\n" +
+            "TYn5iFqPQJhDoSiE8W0CeyAUXyhwWg7l9qiBaA+nI+t1Y307ld4T46x4\n" +
+            "-----END CERTIFICATE-----";
+    private static final String configFile = "config.properties";
 
     @BeforeAll
     public static void setup() throws Exception {
         Properties properties = new Properties();
-        try (InputStream input = MapepireTest.class.getClassLoader().getResourceAsStream(CONFIG_FILE)) {
+        try (InputStream input = MapepireTest.class.getClassLoader().getResourceAsStream(configFile)) {
             if (input == null) {
-                throw new FileNotFoundException("Unable to find " + CONFIG_FILE);
+                throw new FileNotFoundException("Unable to find " + configFile);
             }
             properties.load(input);
         }
@@ -40,22 +64,20 @@ class MapepireTest {
             secrets.put(key, value);
         }
 
-        String host = secrets.get(keys.get(0));
-        String user = secrets.get(keys.get(1));
-        String password = secrets.get(keys.get(2));
-        int port = Integer.parseInt(secrets.get(keys.get(3)));
-
-        creds = new DaemonServer(host, port, user, password);
-        String ca = Tls.getCertificate(creds).get();
-        creds.setCa(ca);
-        invalidCreds = new DaemonServer(host, port, "FAKE_USER", "FAKE_PASSWORD", false);
+        host = secrets.get(keys.get(0));
+        user = secrets.get(keys.get(1));
+        password = secrets.get(keys.get(2));
+        port = Integer.parseInt(secrets.get(keys.get(3)));
     }
 
-    public static DaemonServer getCreds() {
+    public static DaemonServer getCreds() throws Exception {
+        DaemonServer creds = new DaemonServer(host, port, user, password);
+        String ca = Tls.getCertificate(creds).get();
+        creds.setCa(ca);
         return creds;
     }
 
     public static DaemonServer getInvalidCreds() {
-        return invalidCreds;
+        return new DaemonServer(host, port, "FAKE_USER", "FAKE_PASSWORD", false, invalidCA);
     }
 }
--- src/test/java/io/github/mapepire_ibmi/TlsTest.java
+++ src/test/java/io/github/mapepire_ibmi/TlsTest.java
@@ -1,15 +1,47 @@
 package io.github.mapepire_ibmi;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
+
+import java.util.concurrent.ExecutionException;
+
 import org.junit.jupiter.api.Test;
+
+import io.github.mapepire_ibmi.types.DaemonServer;
 
 class TlsTest extends MapepireTest {
     @Test
-    void canGetCertCorrectly() {
-        // TODO:
+    void canGetCertCorrectly() throws Exception {
+        String ca = Tls.getCertificate(MapepireTest.getCreds()).get();
+
+        assertNotNull(ca);
     }
 
     @Test
-    void willFailCorrectly() {
-        // TODO:
+    void failsToGetCertCorrectly() throws Exception {
+        ExecutionException e = assertThrowsExactly(ExecutionException.class, () -> {
+            DaemonServer invalidCreds = MapepireTest.getInvalidCreds();
+            invalidCreds.setHost("FAKE_HOST");
+            Tls.getCertificate(invalidCreds).get();
+        });
+
+        assertEquals(
+                "java.net.ConnectException: Connection refused: connect",
+                e.getMessage());
+    }
+
+    @Test
+    void failsHandshakeWithInvalidCa() throws Exception {
+        ExecutionException e = assertThrowsExactly(ExecutionException.class, () -> {
+            DaemonServer creds = MapepireTest.getCreds();
+            creds.setCa(MapepireTest.invalidCA);
+            SqlJob job = new SqlJob();
+            job.connect(creds).get();
+        });
+
+        assertEquals(
+                "javax.net.ssl.SSLHandshakeException: PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target",
+                e.getMessage());
     }
 }
